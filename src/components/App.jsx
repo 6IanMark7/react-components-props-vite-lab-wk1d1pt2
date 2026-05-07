54ec6a23ee688e6376dd810f913ddb67efe0cbb7
Ian <6markian7@gmail.com>
2026-05-07 16:29:16 +0300

    updated Blog-site code after running npm tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 
 import blogData from "../data/blog";
-import ArticleList from '../BlogComponents/ArticleList';
+import ArticleList from "./ArticleList";
+import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <ArticleList appBlog={ blogData } />
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={ blogData.posts } />
     </div>
   );
 }
